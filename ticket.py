f79dd67258d34028c4e74bace24f681635994a5e
Gisela Bianchi <gisela-bianchi@hotmail.com>
2024-12-01 14:06:55 -0300

    Menu y opcion Salir

---
+++ ticket.py
@@ -0,0 +1,29 @@
+import random
+
+tickets = []
+
+while True:
+    print("\n")
+    print("Hola bienvenido al sistema de Tickets\n")
+    print("==============================")
+    print("1. Generar un Nuevo Ticket   |")
+    print("2. Leer un Ticket            |")
+    print("0. Salir                     |")
+    print("==============================")
+    opcion = int(input("Por favor, selecciona una opcion: "))
+
+    if opcion == 1:  # Alta ticket
+        # Delegar a la persona encargada de la Parte 2
+        pass
+
+    elif opcion == 2:  # Leer ticket
+        # Delegar a la persona encargada de la Parte 3
+        pass
+
+    elif opcion == 0:  # Salir
+        print("Saliendo del programa...")
+        break
+
+    else:
+        print("Opcion no valida")
+
